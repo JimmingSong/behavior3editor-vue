bf6a32678059ede57959e0270f9150f7662c5c17
jimmySong <sljym0611@163.com>
2024-02-04 21:35:53 +0800

    基本功能完成

--- src/editor-next/use-select-hook.ts
+++ src/editor-next/use-select-hook.ts
@@ -1,6 +1,7 @@
 import {EditorInstance} from "./use-editor-hook.ts";
 import {MaybeRef} from "vue";
 import {getTree} from "./use-create-project.ts";
+import * as Mousetrap from "mousetrap";
 
 export function useSelectHook(editor: MaybeRef<EditorInstance>) {
   const getEditor = () => toValue(editor)
@@ -14,9 +15,22 @@ export function useSelectHook(editor: MaybeRef<EditorInstance>) {
     getTree(getEditor()).selection.invertSelection();
   }
 
+  const bindSelectMousetrap = () => {
+    Mousetrap.bind('ctrl+a', onSelectAll)
+    Mousetrap.bind('ctrl+i', onDeselectAll)
+  }
+
+  const unbindSelectMousetrap = () => {
+    Mousetrap.unbind('ctrl+a') // 取消绑定
+    Mousetrap.unbind('ctrl+i') // 取消绑定
+  }
+
   return {
     onSelectAll,
     onDeselectAll,
-    onInvertSelection
+    onInvertSelection,
+
+    bindSelectMousetrap,
+    unbindSelectMousetrap
   }
 }
